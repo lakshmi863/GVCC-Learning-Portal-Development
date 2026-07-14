2a21316dc391410c155823748b1607c5e4c4a618
lakshmi863 <sathya.lakshmipathi@gmail.com>
2026-07-15 02:58:39 +0530

    front api url add

--- Backend/index.js
+++ Backend/index.js
@@ -11,9 +11,22 @@ const videoRoutes = require('./routes/videoRoutes');
 connectDB();
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://gvcc-learning-portal-development-front.onrender.com'
+];
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // URL of your Frontend
-    credentials: true
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
 app.use(express.json());
 
